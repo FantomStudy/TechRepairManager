7135416e846be8d6ad4900749ca2f1bbc75e030a
FantomStudy <144556354+FantomStudy@users.noreply.github.com>
2024-05-27 12:02:26 +0500

    Add files via upload

---
+++ TechLibrary/TechLibrary/Request.cs
@@ -0,0 +1,17 @@
+﻿namespace TechLibrary
+{
+    public class Request
+    {
+        public string Client{ get; set; }
+        public string Comment { get; set; }
+        public string TypeOfService { get; set; }
+        public bool Status { get; set; }
+        public Request(string client, string comment, string typeOfService, bool status)
+        {
+            Client = client;
+            TypeOfService = typeOfService;
+            Comment = comment;
+            Status = status;
+        }
+    }
+}
