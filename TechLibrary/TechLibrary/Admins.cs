a72ae4f68a9e74181715fa3f24ef817a7f1db646
FantomStudy <144556354+FantomStudy@users.noreply.github.com>
2024-05-27 11:16:04 +0500

    Add files via upload

---
+++ TechLibrary/TechLibrary/Admins.cs
@@ -0,0 +1,14 @@
+﻿namespace TechLibrary
+{
+    public class Admins
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public Admins(string userName, string password)
+        {
+            UserName = userName;
+            Password = password;
+        }
+    }
+}
---
+++ TechLibrary/TechLibrary/Services.cs
@@ -0,0 +1,15 @@
+﻿namespace TechLibrary
+{
+    public class Services
+    {
+        public string ServiceName { get; set; }
+        public string Description { get; set; }
+        public int Cost { get; set; }
+        public Services(string name, string description, int cost  ) 
+        {
+            ServiceName = name;
+            Description = description;
+            Cost = cost;
+        }
+    }
+}
